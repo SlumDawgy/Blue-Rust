f823448ee6c08a6ee0581dcd89d8b707a2b30215
Kuroma <105494861+Kur0ma@users.noreply.github.com>
2024-03-20 12:03:52 -0300

    Add files via upload

---
+++ Scripts/Camera.gd
@@ -0,0 +1,28 @@
+extends Camera2D
+
+var player
+
+var speedX = 300
+var speedY = 200
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	player = get_parent().get_node("Player")
+	
+	position = to_local(player.position)
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if player.velocity.x > 0:
+		position.x += speedX * delta
+	elif player.velocity.x < 0:
+		position.x -= speedX * delta
+	position.x = clamp(position.x, player.position.x - 30, player.position.x + 30)
+
+	if player.velocity.y > 0:
+		position.y += speedY * delta
+	elif player.velocity.y < 0:
+		position.y -= speedY * delta
+	position.y = clamp(position.y, player.position.y - 25, player.position.y + 25)
