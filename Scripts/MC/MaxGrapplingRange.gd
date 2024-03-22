c97b85c0bcc640f5bd910736c4862fcbb70a7a85
TLSAxt <65037616+TLSAxt@users.noreply.github.com>
2024-03-22 07:17:18 -0400

    Added Damage Sprites and A folder
    I added damage sprites for the character
    I also added a folder for the sprites.
    
    ***If there are any errors, then it is because of the new folder.  I fixed the ones I got by just added /Sprites to the places where it is looking for a fille.

--- Scripts/MC/MaxGrapplingRange.gd
+++ Scripts/MC/MaxGrapplingRange.gd
@@ -7,8 +7,8 @@ var aiming
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	aim = preload("res://Assets/Aim.png")
-	aiming = preload("res://Assets/Aiming.png")
+	aim = preload("res://Assets/Sprites/Aim.png")
+	aiming = preload("res://Assets/Sprites/Aiming.png")
 	pass # Replace with function body.
 
 
