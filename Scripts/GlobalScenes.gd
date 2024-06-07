956d30630ff9513e4ac495e248db33fbd817cece
Deez Noodles <deeznoodles.official@gmail.com>
2024-06-07 14:08:09 -0300

    SmallChanges

--- Scripts/GlobalScenes.gd
+++ Scripts/GlobalScenes.gd
@@ -1,5 +1,8 @@
 extends Node
 
+func _ready():
+	pass
+
 func loadingScene(sceneID : int, entranceID : int):
 	match sceneID:
 		1:
@@ -14,3 +17,17 @@ func loadingScene(sceneID : int, entranceID : int):
 			pass
 		6:
 			pass
+
+func loadingGame():
+	var player = get_tree().root.get_child(get_tree().root.get_child_count() - 1).find_child("Player")
+	
+	player.position.x = int(SaveSystem.get_var("player:positionX"))
+	player.position.y = int(SaveSystem.get_var("player:positionY"))
+	player.doubleJumpEnabled = bool(SaveSystem.get_var("player:doubleJump"))
+	player.dashEnabled = bool(SaveSystem.get_var("player:dash"))
+	player.parasolEnabled = bool(SaveSystem.get_var("player:parasol"))
+	
+	Dialogues.Prison1_1 = bool(SaveSystem.get_var("Dialogues:Prison1_1"))
+	Dialogues.Prison1_2 = bool(SaveSystem.get_var("Dialogues:Prison1_2"))
+	Dialogues.Prison1_3 = bool(SaveSystem.get_var("Dialogues:Prison1_3"))
+	Dialogues.Prison1_4 = bool(SaveSystem.get_var("Dialogues:Prison1_4"))
