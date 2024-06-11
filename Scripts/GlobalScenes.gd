5fda4f108c5428651cbca074875c7bfb0f7a277a
Kuroma <leoantunesps2@gmail.com>
2024-06-11 19:40:17 -0300

    Done

--- Scripts/GlobalScenes.gd
+++ Scripts/GlobalScenes.gd
@@ -23,9 +23,6 @@ func loadingScene(sceneID : String, entranceID : int):
 	
 	get_tree().paused = false
 
-func loadingSettings():
-	pass
-
 func loadingPlayer(player : Player):
 	player.position.x = int(SaveSystem.get_var("player:positionX"))
 	player.position.y = int(SaveSystem.get_var("player:positionY"))
@@ -37,3 +34,19 @@ func loadingPlayer(player : Player):
 	Dialogues.Prison1_2 = bool(SaveSystem.get_var("Dialogues:Prison1_2"))
 	Dialogues.Prison1_3 = bool(SaveSystem.get_var("Dialogues:Prison1_3"))
 	Dialogues.Prison1_4 = bool(SaveSystem.get_var("Dialogues:Prison1_4"))
+
+func saveSettings():
+	SaveSystem.set_var("settings", {"language":null, "fullscreen":null,"soundMaster":null,"soundFX":null, "soundBGM":null})
+	SaveSystem.set_var("settings:language", TranslationServer.get_locale())
+	SaveSystem.set_var("settings:fullscreen", DisplayServer.window_get_mode())
+	SaveSystem.set_var("settings:soundMaster", AudioServer.get_bus_volume_db(AudioServer.get_bus_index("Master")))
+	SaveSystem.set_var("settings:soundFX", AudioServer.get_bus_volume_db(AudioServer.get_bus_index("SFX")))
+	SaveSystem.set_var("settings:soundBGM", AudioServer.get_bus_volume_db(AudioServer.get_bus_index("BGM")))
+
+func loadingSettings():
+	if SaveSystem.has("settings"):
+		TranslationServer.set_locale(SaveSystem.get_var("settings:language"))
+		AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), SaveSystem.get_var("settings:soundMaster"))
+		AudioServer.set_bus_volume_db(AudioServer.get_bus_index("SFX"), SaveSystem.get_var("settings:soundFX"))
+		AudioServer.set_bus_volume_db(AudioServer.get_bus_index("BGM"), SaveSystem.get_var("settings:soundBGM"))
+		DisplayServer.window_set_mode(SaveSystem.get_var("settings:fullscreen"))
--- Scripts/UI/Main_Menu.gd
+++ Scripts/UI/Main_Menu.gd
@@ -8,12 +8,14 @@ extends Control
 var optionsScene = preload(GlobalPaths.OPTIONS_SCREEN_PATH)
 
 func _ready():
+	Loadings.loadingSettings()
 	DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
 	Input.set_mouse_mode(Input.MOUSE_MODE_CONFINED)
 	$Scenetransition.fadetonormal()
 
 func _on_back_button_pressed():
 	AudioManager.play_sound(buttonClickSFX)
+	Loadings.saveSettings()
 	options.hide()
 	menu.show()
 
