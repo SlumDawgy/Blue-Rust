7ca32a8fce400f629a2e901ed17cadabb83c0530
Deez Noodles <deeznoodles.official@gmail.com>
2024-06-18 13:12:24 -0300

    FinishedBridge

--- Scripts/Objects/BreakingBridge.gd
+++ Scripts/Objects/BreakingBridge.gd
@@ -20,13 +20,17 @@ func _on_start_breaking_body_entered(body : Player):
 		startBreaking = true
 		sprite.play("BeginBreak")
 		particles.emitting = true
-		AudioManager.play_sound(brokeAudio)
+		AudioManager.play_sound(breakingAudio)
+		
+		if sprite.visible == false:
+			await get_tree().create_timer(8).timeout
+			queue_free()
 
 
 func _on_broke_body_entered(body : Player):
 	startBreaking = true
 	sprite.visible = false
 	brokeParticle.emitting = true
-	AudioManager.play_sound(breakingAudio)
+	AudioManager.play_sound(brokeAudio)
 	set_collision_layer_value(4, false)
 	set_collision_mask_value(1, false)
