decefe6186f422fce6238bea7d308f38a760f2df
TLSAxt <65037616+TLSAxt@users.noreply.github.com>
2024-09-04 09:07:44 -0400

    Update SaveManager.gd

--- Scripts/SaveManager.gd
+++ Scripts/SaveManager.gd
@@ -2,10 +2,13 @@ extends Node
 
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func save_game():
+	# All save information should be here (Scenes, dialogue, player stuff, etc.)
+	# When saving the game this should be called from the save script.  It just keeps it in a more uniformed place
+	# My belief is taht it will be more a cleaner system in the end
+	pass
+	
+func load_game():
+	# All loading information should be done here
+	# At when loading the game This should be called 
 	pass
