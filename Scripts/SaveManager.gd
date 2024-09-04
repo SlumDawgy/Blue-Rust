2639af71aedc71b63a313b78dedcfb50f3881867
TLSAxt <65037616+TLSAxt@users.noreply.github.com>
2024-09-04 09:11:01 -0400

    Added autoload

--- Scripts/SaveManager.gd
+++ Scripts/SaveManager.gd
@@ -4,6 +4,7 @@ extends Node
 # Called when the node enters the scene tree for the first time.
 func save_game():
 	# All save information should be here (Scenes, dialogue, player stuff, etc.)
+	# You should be able to just put what is already in the Save Point Script here
 	# When saving the game this should be called from the save script.  It just keeps it in a more uniformed place
 	# My belief is taht it will be more a cleaner system in the end
 	pass
